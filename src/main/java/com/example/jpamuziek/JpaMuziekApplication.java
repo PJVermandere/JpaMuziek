8f00840cd9a8e46ef1ea63058a4295e160474389
main <prince.vermandere@outlook.be>
2021-11-23 15:57:32 +0100

    init

---
+++ src/main/java/com/example/jpamuziek/JpaMuziekApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamuziek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMuziekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMuziekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpamuziek/JpaMuziekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamuziek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMuziekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
