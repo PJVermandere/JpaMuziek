fab24224df85d3cfcfbda5fef8b1949bf291170d
main <prince.vermandere@outlook.be>
2021-11-24 14:41:48 +0100

    Test toegevoegd: findAll

--- src/test/java/com/example/jpamuziek/repositories/JpaAlbumRepositoryTest.java
+++ src/test/java/com/example/jpamuziek/repositories/JpaAlbumRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.example.jpamuziek.repositories;
 
 import com.example.jpamuziek.domain.Album;
+import com.example.jpamuziek.domain.Artiest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,15 +15,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(JpaAlbumRepository.class)
 class JpaAlbumRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
     private final JpaAlbumRepository repository;
+    private final EntityManager manager;
     private static final String ALBUMS = "albums";
 
-    JpaAlbumRepositoryTest(JpaAlbumRepository repository) {
+    JpaAlbumRepositoryTest(JpaAlbumRepository repository, EntityManager manager) {
         this.repository = repository;
+        this.manager = manager;
     }
 
     @Test
     void findAll() {
-        assertThat(repository.findAll())
+        var albums = repository.findAll();
+        manager.clear();
+        assertThat(albums)
                 .hasSize(countRowsInTable(ALBUMS));
+        assertThat(albums)
+                .extracting(Album::getArtiest)
+                .extracting(Artiest::getNaam);
+
     }
 }
