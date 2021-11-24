76d8e8b283a4c3701590e07e28fe73531908169c
main <prince.vermandere@outlook.be>
2021-11-24 14:42:40 +0100

    EntityGraph toegevoegd aan NamedQuery in findall()

--- src/main/java/com/example/jpamuziek/repositories/JpaAlbumRepository.java
+++ src/main/java/com/example/jpamuziek/repositories/JpaAlbumRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static com.example.jpamuziek.domain.Album.ALBUM_FINDARTIEST;
+
 @Repository
 public class JpaAlbumRepository implements AlbumRepository {
     private final EntityManager manager;
@@ -17,6 +19,7 @@ public class JpaAlbumRepository implements AlbumRepository {
     @Override
     public List<Album> findAll() {
         return manager.createNamedQuery("Album.findAll")
+                .setHint("javax.persistence.loadgraph", manager.createEntityGraph(ALBUM_FINDARTIEST))
                 .getResultList();
     }
 }
