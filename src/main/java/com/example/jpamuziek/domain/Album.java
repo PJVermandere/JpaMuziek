0bcac75aa7647d6ffbba1a60e3bd236e6cdce41c
main <prince.vermandere@outlook.be>
2021-11-24 11:26:49 +0100

    New entity class Album

---
+++ src/main/java/com/example/jpamuziek/domain/Album.java
@@ -0,0 +1,63 @@
+package com.example.jpamuziek.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity @Table(name = "albums")
+public class Album {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "artiestId")
+    private Artiest artiest;
+    private String naam;
+    private int jaar, score;
+    private long barcode;
+
+    public Album() {
+    }
+
+    public Album(long id, Artiest artiest, String naam, int jaar, int score, long barcode) {
+        this.id = id;
+        this.artiest = artiest;
+        this.naam = naam;
+        this.jaar = jaar;
+        this.score = score;
+        this.barcode = barcode;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Artiest getArtiest() {
+        return artiest;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getJaar() {
+        return jaar;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public long getBarcode() {
+        return barcode;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Album album && barcode == album.barcode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(barcode);
+    }
+}
