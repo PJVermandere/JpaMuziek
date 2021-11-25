713ac8597c666bba09a47fc2c019d69238317e86
main <prince.vermandere@outlook.be>
2021-11-25 11:18:38 +0100

    Added controller to display albumInfo.html

--- src/main/java/com/example/jpamuziek/controllers/IndexController.java
+++ src/main/java/com/example/jpamuziek/controllers/IndexController.java
@@ -3,7 +3,9 @@ package com.example.jpamuziek.controllers;
 import com.example.jpamuziek.services.AlbumService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,6 +20,11 @@ public class IndexController {
     @GetMapping
     public ModelAndView indexController(){
         var MVC = new ModelAndView("index", "albums", service.getAllAlbums());
+        return MVC;
+    }
+    @GetMapping("/album/{id}")
+    public ModelAndView albumInfo(@PathVariable long id){
+        var MVC = new ModelAndView("albumInfo", "album", service.findAlbumById(id));
         return MVC;
     }
 }
