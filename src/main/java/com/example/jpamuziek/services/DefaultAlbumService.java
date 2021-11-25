f41ed5c577f85c9b565efd8636f72eb512b870a2
main <prince.vermandere@outlook.be>
2021-11-25 11:17:11 +0100

    Added functionality: Optional<Album> findAlbumById(long id)

--- src/main/java/com/example/jpamuziek/services/DefaultAlbumService.java
+++ src/main/java/com/example/jpamuziek/services/DefaultAlbumService.java
@@ -1,6 +1,7 @@
 package com.example.jpamuziek.services;
 
 import com.example.jpamuziek.domain.Album;
+import com.example.jpamuziek.exceptions.AlbumNietGevondenException;
 import com.example.jpamuziek.repositories.AlbumRepository;
 import com.example.jpamuziek.repositories.JpaAlbumRepository;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,10 @@ public class DefaultAlbumService implements AlbumService {
     @Override
     public List<Album> getAllAlbums() {
         return repository.findAll();
+    }
+
+    @Override
+    public Album findAlbumById(long id) {
+        return repository.findById(id).orElseThrow(AlbumNietGevondenException::new);
     }
 }
